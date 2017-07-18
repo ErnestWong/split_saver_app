110dcb820a53d25abdc07fac1372a671e56c49c3
davyuu <davidyu_2005@hotmail.com>
2017-07-18 15:57:45 -0400

    Handle cookies

--- app/src/main/java/com/noname/splitsaver/Network/NetworkManager.java
+++ app/src/main/java/com/noname/splitsaver/Network/NetworkManager.java
@@ -2,6 +2,13 @@ package com.noname.splitsaver.Network;
 
 import com.noname.splitsaver.Models.Transaction;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -14,9 +21,27 @@ public class NetworkManager {
     private static SplitSaverService service;
 
     public static void init() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .cookieJar(new CookieJar() {
+                    List<Cookie> cookies;
+
+                    @Override
+                    public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+                        this.cookies = cookies;
+                    }
+
+                    @Override
+                    public List<Cookie> loadForRequest(HttpUrl url) {
+                        if (cookies != null)
+                            return cookies;
+                        return new ArrayList<>();
+                    }
+                }).build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(baseUrl)
+                .client(client)
                 .build();
 
         service = retrofit.create(SplitSaverService.class);
